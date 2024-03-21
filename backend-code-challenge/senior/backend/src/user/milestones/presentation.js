1223348d83a2e24b20cbaecbaaab126accb132db
Owolabi Bassit <52262467+breellz@users.noreply.github.com>
2024-03-21 12:37:12 +0100

    Add comments

--- backend-code-challenge/senior/backend/src/user/milestones/presentation.js
+++ backend-code-challenge/senior/backend/src/user/milestones/presentation.js
@@ -12,13 +12,16 @@ const {
 const INVALID_FAVOURITES_ERROR = 'Invalid favourites';
 
 module.exports = async (req, res, context) => {
+  // Get the instance and favourites from the context
 
   const { instance } = context;
   const { favourites } = instance.dataValues;
+  // Throw an error if the favourites are invalid
 
   if (!favourites || !Array.isArray(favourites)) {
     throw new Error(INVALID_FAVOURITES_ERROR);
   }
+  // Fetch the details of the favourite characters
 
   const favouritesDetails = await Promise.all(
     favourites.map((id) =>
@@ -32,9 +35,11 @@ module.exports = async (req, res, context) => {
     ),
   );
 
+  // Update the instance with the fetched details
 
   context.instance.dataValues.favouritesDetails = favouritesDetails;
 
-  
+  // Continue to the next middleware
+
   return context.continue;
 };
