139dc05daf319180210492a6a7918466935cf2a0
Owolabi Bassit <52262467+breellz@users.noreply.github.com>
2024-03-21 12:22:31 +0100

    Refactor authentication.js to handle user slugs

--- backend-code-challenge/senior/backend/src/user/milestones/authentication.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authentication.js
@@ -1,16 +1,38 @@
 'use strict';
 
-// const {
-//   Errors: { BadRequestError },
-// } = require('finale-rest');
+const {
+  Errors: { BadRequestError },
+} = require('finale-rest');
 
-// const {
-//   API: {
-//     HEADERS: { X_SLUG },
-//     SLUGS: { MYSELF },
-//   },
-// } = require('../../config');
+const {
+  API: {
+    HEADERS: { X_SLUG },
+    SLUGS: { MYSELF },
+  },
+} = require('../../config');
 
 module.exports = (req, res, context) => {
+  let slug = req.params.slug;
+  if (!slug || slug === MYSELF) {
+    slug = req.header(X_SLUG);
+  }
+
+  if (!slug) {
+    throw new BadRequestError(MISSING_USER_SLUG_ERROR);
+  }
+
+  const decodedSlug = Buffer.from(slug, 'base64').toString();
+  if (decodedSlug.length === 0) {
+    throw new BadRequestError('Invalid user slug');
+  }
+
+  if (decodedSlug === MYSELF) {
+    context.slug = MYSELF;
+  } else {
+    context.slug = null;
+  }
+
+  req.params.slug = decodedSlug;
+  
   return context.continue;
 };
