d2e8e3f28f4c2ae3487e85050f93bcddd21d1c51
Owolabi Bassit <52262467+breellz@users.noreply.github.com>
2024-03-21 12:33:43 +0100

    Add comments

--- backend-code-challenge/senior/backend/src/user/milestones/authorization.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authorization.js
@@ -12,11 +12,16 @@ const {
 } = require('../../config');
 const WRONG_API_KEY_ERROR = 'Wrong API key';
 module.exports = (req, res, context) => {
+   // Get the API key from the request headers
+
   const apiKey = req.header(X_API_KEY);
+
+  // Throw an error if the API key is wrong
 
   if (apiKey !== KEY) {
     throw new ForbiddenError(WRONG_API_KEY_ERROR);
   }
 
+  // Continue to the next middleware
   return context.continue;
 };
